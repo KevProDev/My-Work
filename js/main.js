7917eb1fdf2cbad1764846ed3fe81fd09332ec48
developerXdecoded <34496647+developerXdecoded@users.noreply.github.com>
2019-02-21 19:01:37 -0600

    no message

--- js/main.js
+++ js/main.js
@@ -7,13 +7,17 @@ $(document).ready(function () {
     /* Check the location of each desired element */
     $('.fadein').each(function (i) {
 
-      var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+      var bottom_of_object = $(this).offset().top + $(this).outerHeight() - 700;
       var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+
+      console.log("bottom of window",bottom_of_window);
+      console.log("bottom of object",bottom_of_object);
 
       /* If the object is completely visible in the window, fade it it */
       if (bottom_of_window > bottom_of_object) {
 
-        $(this).animate({ 'opacity': '1' }, 300);
+        $(this).animate({ 'opacity': '1' }, 1000);
 
       }
 
